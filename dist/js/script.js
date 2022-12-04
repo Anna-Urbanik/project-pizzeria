5e4f02261ee68fc67f8c902d2e45bce4a68c9c8b
Anna Urbanik <urbanik.ania@gmail.com>
2022-12-04 15:46:56 +0100

    Correct accordion

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,17 +52,18 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
-  class Product{
-    constructor(id, data){
+  class Product {
+    constructor(id, data) {
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
 
-    renderInMenu(){
+    renderInMenu() {
       const thisProduct = this;
 
       /* generate HTML based on template */
@@ -75,50 +76,61 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
-    initAccordion(){
+    getElements() {
       const thisProduct = this;
 
-    /* find the clickable trigger (the element that should react to clicking) */
-    const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-
-    /* START: add event listener to clickable trigger on event click */
-    thisProduct.clickableTrigger.addEventListener('click', function(event) {
-      console.log('clicked');
-      /* prevent default action for event */
-      event.preventDefault();
-      /* find active product (product that has active class) */
-      //thisProduct.element.toggle = select.menuProduct.clickable   if(activeProduct)
-      //const activeProduct = document.querySelector()
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-
-      /* toggle active class on thisProduct.element */
-    });
-
-  }
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
-  };
+
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /*find the clickable trigger (the element that should react to clicking) */
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: add event listener to clickable trigger on event click */
+      thisProduct.accordionTrigger.addEventListener('click', function (event) {
+        console.log('clicked');
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct != null && activeProduct !=thisProduct.element){  activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
+
+    }
+  }
+
 
   const app = {
-    initMenu: function(){
+    initMenu: function () {
       //const testProduct = new Product();
       //console.log('testProduct:', testProduct);
       const thisApp = this;
 
       console.log('thisApp.data:', thisApp.data);
 
-      for(let productData in thisApp.data.products){
+      for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
     },
 
-    initData: function(){
+    initData: function () {
       const thisApp = this;
-  
+
       thisApp.data = dataSource;
       console.log(thisApp.data);
     },
-        
-    init: function(){
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
@@ -130,5 +142,5 @@
     },
   };
 
-   app.init();
+  app.init();
 }
